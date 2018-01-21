f4204b000ab55f673f91b9fc1bbef67da1dee0d4
silveste <silveste@mailbox.org>
2018-01-21 17:58:09 +0000

    The web developer bootcamp, 26.249 YelpCamp: creating campgrounds

--- the-web-developer-bootcamp/YelpCamp/app.js
+++ the-web-developer-bootcamp/YelpCamp/app.js
@@ -1,9 +1,21 @@
-/* global require */
+/*jslint node: true */
+"use strict";
 /*Dev section*/
 var faker =require("faker");
+var campgrounds = [];
+while (campgrounds.length < 5){
+  var campground = {
+    name: faker.commerce.productName(),
+    image: faker.image.image()
+  };
+  campgrounds.push(campground);
+}
 /*End dev section*/
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
@@ -11,15 +23,19 @@ app.get("/", function(req, res){
 });
 
 app.get("/campgrounds", function(req, res){
-  var campgrounds = [];
-  while (campgrounds.length < 5){
-    var campground = {
-      name: faker.commerce.productName(),
-      image: faker.image.image()
-    };
-    campgrounds.push(campground);
-  }
   res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = {name:name, image: image};
+  campgrounds.push(newCampground);
+  res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req,res){
+  res.render("new");
 });
 
 app.listen(3000, function(){
