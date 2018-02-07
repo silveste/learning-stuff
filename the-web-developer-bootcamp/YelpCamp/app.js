50a4d6d8604e64d57d5bcdd6488195e7f0ab2f47
Antonio Fernandez <silveste@mailbox.org>
2018-02-07 21:37:57 +0000

    The web developer bootcamp, 31.279-289 YelpCamp comments

--- the-web-developer-bootcamp/YelpCamp/app.js
+++ the-web-developer-bootcamp/YelpCamp/app.js
@@ -1,28 +1,23 @@
 /*jslint node: true */
 "use strict";
 
-/*DEV SECTION ----------------------------------------------------------------*/
-var faker =require("faker");
-function fakeCampground (){
-  return {
-    name: faker.commerce.productName(),
-    image: faker.image.image(),
-    description: faker.lorem.paragraph()
-  };
-}
-
-/*END DEV SECTION ------------------------------------------------------------*/
-
 
 /* CONFIG SECTION ------------------------------------------------------------*/
 // required packages
 var express         = require("express"),
     app             = express(),
     bodyParser      = require("body-parser"),
-    mongoose        = require("mongoose");
+    mongoose        = require("mongoose"),
+    Campground      = require("./models/campground"),
+    Comment         = require("./models/comment"),
+    User            = require("./models/user"),
+    seedDB          = require("./seeds");
 
 //Using body-parser to parse post requests
 app.use(bodyParser.urlencoded({extended: true}));
+
+//Set public directory to server assests
+app.use(express.static(__dirname + "/public"));
 
 //Set ejs as default template egine, avoiding write extension when call files
 app.set("view engine", "ejs");
@@ -30,56 +25,35 @@ app.set("view engine", "ejs");
 //Conecting database
 mongoose.connect("mongodb://localhost/yelpcamp");
 
-//Schemas
-var campgroundSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  description: String
-});
-var Campground = mongoose.model("Campground", campgroundSchema);
+//Seeding database for testing purposes
+seedDB();
+
 /*END CONFIG SECTION ---------------------------------------------------------*/
 
-/*TESTING ---------------------------------------------------------*/
-//Creating a minimum of 10 items in the DB
-Campground.find({}, function(err, campgrounds){
-  if(err){
-    console.log(err)
-    return -1;
-  } else {
-    console.log("Setting campgrounds to a minimum of 10 for testing puposes");
-    console.log("Initial campgrounds:" + campgrounds.length);
-    for (var i = campgrounds.length; i < 10; i++) {
-      Campground.create(fakeCampground(), function (err, campground){
-        if(err){
-          console.log(err);
-        } else {
-          console.log("New campground created");
-          console.log(campground);
-        }
-      });
-    }
-  }
-});
 
-/*END TESTING ---------------------------------------------------------*/
 
 app.get("/", function(req, res){
   res.render("landing");
 });
 
+// ===========================
+// Campgrounds routes
+// ===========================
+
+//INDEX - show all campgrounds
 app.get("/campgrounds", function(req, res){
   
   Campground.find({},function(err,foundCampgrounds){
     if(err){
       console.log(err);
     } else {
-      res.render("index", {campgrounds: foundCampgrounds});
+      res.render("campgrounds/index", {campgrounds: foundCampgrounds});
     }
   });
   
 });
 
-//INDEX - show all campgrounds
+//CREATE - create a new campground
 app.post("/campgrounds", function(req, res){
   var name = req.body.name;
   var image = req.body.image;
@@ -96,20 +70,60 @@ app.post("/campgrounds", function(req, res){
   res.redirect("/campgrounds");
 });
 
-//NEW - Page that creates a new campground
+//NEW - Page that show from to create a new campground
 app.get("/campgrounds/new", function(req,res){
-  res.render("new");
+  res.render("campgrounds/new");
 });
 
-//SHOW - Page that shows detailed info about a campground
+//SHOW - Page that shows detailed info about a specific campground
 app.get("/campgrounds/:id", function(req,res){
-  Campground.findById(req.params.id, function(err, campground){
+  Campground.findById(req.params.id).populate("comments").exec(function(err, campground){
     if(err){
       console.log(err);
     }else{
-      res.render("show", {campground: campground});
+      res.render("campgrounds/show", {campground: campground});
     }
   });
+});
+
+// ===========================
+// Comments routes
+// ===========================
+
+// NEW - creates a new comment inside a specific campground
+app.get("/campgrounds/:id/comments/new", function(req,res){
+  Campground.findById(req.params.id, function(err, foundCampground){
+    if(err){
+      console.log(err);
+    }else{
+        res.render("comments/new", {campground: foundCampground});
+    }
+  })
+})
+
+// CREATE - create a new comment inside a specific campground
+app.post("/campgrounds/:id/comments", function(req, res){
+  Campground.findById(req.params.id, function(err, foundCampground) {
+      if(err){
+        console.log(err);
+        res.redirect("/campgrounds")
+      }else{
+        Comment.create(req.body.comment, function(err,newComment){
+          if(err){
+            console.log(err);
+          }else{
+            foundCampground.comments.push(newComment);
+            foundCampground.save(err, function(){
+              if(err){
+                console.log(err)
+              }else{
+                res.redirect("/campgrounds/" + foundCampground._id)
+              }
+            });
+          }
+        })
+      }
+  })
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
