b6aecfc036d36e7b39c041a6596cafa4c930a306
Antonio Fernandez <silveste@mailbox.org>
2018-03-14 21:21:36 +0000

    Complete Java Masterclass: section 10 84-86 Abstract classes

---
+++ complete-java-masterclass/84-86_Abstract_classes/src/com/silveste/Node.java
@@ -0,0 +1,38 @@
+package com.silveste;
+
+public class Node extends ListItem {
+    public Node(Object value){
+        super(value);
+    }
+
+    @Override
+    ListItem next() {
+        return this.nextLink;
+    }
+
+    @Override
+    ListItem setNext(ListItem item) {
+        this.nextLink = item;
+        return this.nextLink;
+    }
+
+    @Override
+    ListItem prev() {
+        return this.prevLink;
+    }
+
+    @Override
+    ListItem setPrev(ListItem item) {
+        this.prevLink = item;
+        return this.prevLink;
+    }
+
+    @Override
+    int compareTo(ListItem item) {
+        if (item != null){
+            return((String) super.getValue()).compareTo((String) item.getValue());
+        } else {
+            return -1;
+        }
+    }
+}
