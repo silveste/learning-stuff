e75f443411ae98710651af0135ae30b1d12690f4
Antonio Fernandez <silveste@mailbox.org>
2018-10-17 22:07:07 +0100

    (ammend) add bd path comment

--- the-advanced-web-developer-bootcamp/s34-35_warbler_backend/models/index.js
+++ the-advanced-web-developer-bootcamp/s34-35_warbler_backend/models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.set('debug', (process.env.NODE_ENV !== 'production'));
 mongoose.Promise = Promise;
-mongoose.connect('mongodb:' + process.env.DB_PATH, {
+mongoose.connect('mongodb:' + process.env.DB_PATH, { //Format: //localhost/warbler
   keepAlive: true,
   useNewUrlParser: true
   // useMongoClient: true //userMongoClient is not neccessary in mongoose 5.x
