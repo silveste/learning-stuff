852968ee3afd4fdf4a63c65655eb08ad69576b0d
silveste <silveste@mailbox.org>
2019-01-31 16:32:16 +0000

    Java EE with Vaadin, Spring Boot and Maven: sec 8 lect 96 Integrating with Vaadin

---
+++ java-ee-with-vaadin-spring-boot-and-maven/univers/univers-ui/src/main/java/com/silveste/ui/commons/UniverseMainUI.java
@@ -0,0 +1,26 @@
+package com.silveste.ui.commons;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+@SpringUI(path=UniverseMainUI.NAME)
+@Title("U N I V E R S")
+@Theme("valo")
+public class UniverseMainUI extends UI {
+
+    public static final String NAME = "/ui";
+
+    @Override
+    protected void init(VaadinRequest request) {
+        VerticalLayout rootLayout = new VerticalLayout();
+
+        rootLayout.addComponent(new Label("Yuhuuuuuuu!!!"));
+
+        setContent(rootLayout);
+    }
+}
