7b519f90913f6ce7416c8bff7d12ab034eabfd19
silveste <silveste@mailbox.org>
2019-02-04 11:59:40 +0000

    React native - The practical guide: 2.23 Working on the app: Adding a Textinput

--- react_native_the_practical_guide/sec.2-lec.12-39_diving_into_the_basics/rncourse/App.js
+++ react_native_the_practical_guide/sec.2-lec.12-39_diving_into_the_basics/rncourse/App.js
@@ -8,7 +8,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -19,12 +19,29 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  state = {
+    placeName: ''
+  };
+
+  placeNameChangeHandler = val => {
+    this.setState({
+      placeName: val
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text>test</Text>
+        <TextInput
+          style = {{
+              width: 300,
+              borderColor: "black",
+              borderWidth: 1
+            }}
+          value={this.state.placeName}
+          onChangeText = {this.placeNameChangeHandler}/>
       </View>
     );
   }
@@ -36,15 +53,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
