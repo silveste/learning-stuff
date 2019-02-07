8ce79cef91c24d981181944e27e8565420afbbf0
Silveste <silveste@mailbox.org>
2019-02-07 23:53:08 +0000

    Webpack 4: Beyond the Basics: sect.2-4 Our first loader for css

--- webpack4_beyond_the_basics/config/webpack.dev.js
+++ webpack4_beyond_the_basics/config/webpack.dev.js
@@ -18,8 +18,33 @@ module.exports = {
     //the js file
     publicPath: "/"
   },
+  //config for web-dev-server
   devServer: {
     //wen running webpack-dev-server everything in dist folder will be served
     contentBase: "dist"
+  },
+  module: {
+    //Rules that webpack will use when encounter with a file with different
+    //language than js
+    rules: [
+      {
+        //test parameter points to a regex that catch the file(s) target
+        test: /\.css$/,
+        //use parameter points to an array with the loaders that will manage
+        //the language contained in the files
+        //webpack will run the loaders in inverse order, in this case css-loader
+        //first and style-loader second
+        use: [
+          {
+            //loader responsible to inject the css into the html
+            loader: "style-loader"
+          },
+          {
+            //loader responsible for linting the file
+            loader: "css-loader"
+          }
+        ]
+      }
+    ]
   }
 }
--- webpack4_beyond_the_basics/src/main.js
+++ webpack4_beyond_the_basics/src/main.js
@@ -1,1 +1,1 @@
-alert("Yuhuuuu!!")
+require("./main.css")
