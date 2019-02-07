56f7b82f86c4f19364af88093f04f352af8fc85a
silveste <silveste@mailbox.org>
2019-02-07 17:23:04 +0000

    Java Spring Tutorial Masterclass, section 5 19-22 Project setup and using spring container

---
+++ java-spring-tutorial-masterclass/sec5.19-42_spring5_multi_module_spring_project/core/src/main/java/com/silveste/Main.java
@@ -0,0 +1,33 @@
+package com.silveste;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    private static final String CONFIG_LOCATION = "beans.xml";
+
+    public static void main(String[] args) {
+        log.info("Guess The Number Game");
+
+        // Create the context (container)
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+
+        //To instatiate a class, context.getBean needs the id of the class specified in CONFIG_LOCATION
+        NumberGenerator numberGenerator = context.getBean("numberGenerator", NumberGenerator.class);
+
+        //Call method next() to get a random number
+        int number = numberGenerator.next();
+
+        //log generated number
+        //Curly braces are replaced by number due to internal configuration of slf4j
+        log.info("number = {}", number);
+
+        //Close context (container)
+        context.close();
+    }
+}
