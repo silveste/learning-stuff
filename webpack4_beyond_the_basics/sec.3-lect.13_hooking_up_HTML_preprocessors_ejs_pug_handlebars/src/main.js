b7781e124707afe900fcdc5d094801da640eee33
Silveste <silveste@mailbox.org>
2019-02-14 00:26:46 +0000

    Webpack 4 beyond the basics: section 3 lec 13 Hooking UP HTML Preprocessors EJS/Pug/Handlebars

---
+++ webpack4_beyond_the_basics/sec.3-lect.13_hooking_up_HTML_preprocessors_ejs_pug_handlebars/src/main.js
@@ -0,0 +1,24 @@
+//babel runtime regenerator allows transforming the code for older browsers
+require("babel-runtime/regenerator");
+//webpack-hot-middleware/client provides the neccesary js code to set the websocket
+//connection, allowing webpack to reload the webserver on changes
+//reload=true allows html-webpack-plugin to reload when changing the HTML
+require("webpack-hot-middleware/client?reload=true");
+//requires two loaders css-loader to lint the file and style-loader
+//to insert the code in the html
+require("./main.css");
+//Requires html-loader, extract-loader and file-loader
+require("./index.html");
+
+/*Setting Up Babel*/
+//Not all browsers support arrow functions, however babel will transpile for old browsers
+//babel-plugin-transform-es2015-arrow-functions plugin is required to make the following
+//code work accross old browsers
+/*
+var a = async (args) => {
+  const {a, b} = args
+  await console.log("Hello from an arrow function",a ,b);
+  console.log("Done");
+}
+a({a: 1, b: 2});
+*/
