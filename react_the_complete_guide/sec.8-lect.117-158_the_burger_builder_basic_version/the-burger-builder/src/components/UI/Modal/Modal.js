6615aded1ed084cb0e5dd49574b0be6fd0749bf4
Antonio Fernandez <silveste@mailbox.org>
2020-05-23 18:30:50 +0100

    React the complete guide: 8 lect.156 Changing folder structure

--- react_the_complete_guide/sec.8-lect.117-158_the_burger_builder_basic_version/the-burger-builder/src/components/UI/Modal/Modal.js
+++ react_the_complete_guide/sec.8-lect.117-158_the_burger_builder_basic_version/the-burger-builder/src/components/UI/Modal/Modal.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-  <>
-    <Backdrop show={props.show} clicked={props.close}/>
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0',
-      }}
-    >
-      {props.children}
-    </div>
-  </>
-);
+class Modal extends Component {
+  shouldComponentUpdate (nextProps) {
+    return nextProps.show !== this.props.show;
+  }
+  /*
+  componentDidUpdate() {
+    console.log('Modal updated');
+  }
+  */
+  render() {
+    return (
+      <>
+        <Backdrop show={this.props.show} clicked={this.props.close}/>
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0',
+          }}
+        >
+          {this.props.children}
+        </div>
+      </>
+    )
+  }
+}
 
-export default modal;
+export default Modal;
